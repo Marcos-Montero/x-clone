285150b71f6cc4cc8bb51cec03f1f26ed2ff50f2
Marcos M. Rod <66304414+MarcosMRod@users.noreply.github.com>
2023-09-21 21:36:22 +0200

    fix callback for build

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,8 +1,6 @@
 import { cookies } from 'next/headers';
-import {
-  type NextRequest,
-  NextResponse,
-} from 'next/server';
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
 
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
 
@@ -13,7 +11,7 @@ export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url)
   const code = requestUrl.searchParams.get('code')
   if (!code) {
-    return
+    return new NextResponse('Missing code', { status: 400 })
   }
   const supabase = createRouteHandlerClient({ cookies })
   await supabase.auth.exchangeCodeForSession(code)
